595feae72e95098ecd5d3f36c4205d9e5b6b22af
AmgadFarag <amagido2@gmail.com>
2023-07-09 17:48:44 +0300

    minor edit

--- Database Query/database_query.sql
+++ Database Query/database_query.sql
@@ -1,2 +1,2 @@
 SELECT user_id, username, training_id, training_date, count FROM
-User u INNER JOIN training_details t ON u.user_id = t.user_id;
+User u INNER JOIN Training_details t ON u.user_id = t.user_id;
