5e4c9945568935f81d99ea24915dbcc444ed8cc2
AmgadFarag <amagido2@gmail.com>
2023-07-10 20:07:21 +0300

    Spring project init

---
+++ System Design/filetree/src/main/java/amgadfarag/digivisions/filetree/FileTreeApplication.java
@@ -0,0 +1,13 @@
+package amgadfarag.digivisions.filetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTreeApplication.class, args);
+	}
+
+}
---
+++ System Design/filetree/src/test/java/amgadfarag/digivisions/filetree/FileTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package amgadfarag.digivisions.filetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
